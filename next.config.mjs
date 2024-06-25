ed120ee04f4f9808eff35579fc15194ac85b6da8
Seokmin Lee <essemfly@gmail.com>
2024-06-25 18:30:07 +0900

    Fix path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: '',
-    basePath: '',
+    basePath: '/mylists',
+    assetPrefix: '/mylists/',  
     trailingSlash: true,
     images: {
         unoptimized: true,
